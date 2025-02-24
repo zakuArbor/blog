81072de007fd2623bc5f0db53bde11e2ee1900c7
Ju Hong Kim <nabimoon1234@gmail.com>
2025-02-23 22:41:51 -0500

    [micro] 2024 Microblog edition

---
+++ assets/micro/js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  let menu = document.getElementById("header-nav");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
